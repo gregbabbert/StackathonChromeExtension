dd48ea2bf41007c28a014a77b57aac9b21d9209b
Greg <gdbabbert@gmail.com>
2021-12-14 11:37:52 -0500

    get page text on click

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// alert('hi')
+let title = document.querySelector('h1')
+title.innerHTML = 'fart'
+
+let body = document.querySelector('body')
+body.addEventListener("click", (evt) => {
+    let value = evt.target.innerHTML;
+    alert(value)
+})
