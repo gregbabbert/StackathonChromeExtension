a5c88f2346ba9f2733c9f6a1161060361f852e19
Greg <gdbabbert@gmail.com>
2021-12-14 17:39:47 -0500

    work on getting audio play to work

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 let title = document.querySelector('h1')
 title.innerHTML = 'fart'
 
-let body = document.querySelector('body')
-body.addEventListener("click", (evt) => {
-    let value = evt.target.innerHTML;
-    alert(value)
+let textAreaEle = document.createElement('textarea')
+let playDiv = document.createElement('div')
+let playButton = document.createElement('button')
+playButton.innerHTML = 'Play'
+let pauseButton = document.createElement('button')
+pauseButton.innerHTML = 'Pause'
+let stopButton = document.createElement('button')
+stopButton.innerHTML = 'Stop'
+
+let body = document.querySelector('body.popup')
+body.style.backgroundColor = '#A9D5EA'
+let pageBody = document.querySelectorAll('body')
+body.append(textAreaEle)
+body.append(playDiv)
+playDiv.append(playButton)
+playDiv.append(pauseButton)
+playDiv.append(stopButton)
+
+playButton.addEventListener("click", () => {
+    alert('play')
+})
+
+
+pageBody[1].addEventListener("click", (evt) => {
+    let pValue = evt.target.textContent;
+    textAreaEle.value = pValue
+    alert(pValue)
 })
